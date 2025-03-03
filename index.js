8a06be7c1f34e4a0c5164602bb0475e7975a26cc
DavidM <169084424+codeby-david@users.noreply.github.com>
2025-03-03 16:32:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ app.post('/api/products', async (req, res) => {
     await Product.create(req.body);
     res.status(200).json({message: 'Product created successfully'});
   } catch (err) {
-    res.status(500).json({ message: err.message });
+    res.status(500).json({ err: message.err});
   }
 });
 
@@ -104,6 +104,6 @@ app.delete('/api/products/:id', async (req, res) => {
     }
   }
   catch(err){
-    res.status(500).json({message:message.err});
+    res.status(500).json({err:message.err});
   }
 });
